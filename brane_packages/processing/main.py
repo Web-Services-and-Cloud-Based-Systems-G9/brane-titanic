667478eb6252cfc4c66241e1c17c4ba2847029bd
lkuffo <lkuffo@espol.edu.ec>
2022-05-31 02:07:59 +0200

    Added viz package and first brainscript pipeline

--- brane_packages/processing/main.py
+++ brane_packages/processing/main.py
@@ -6,6 +6,7 @@ from sklearn.tree import DecisionTreeClassifier
 from typing import List, Tuple
 import pandas as pd
 import numpy as np
+import uuid
 
 
 def read_datasets(train_file: str, test_file: str) -> Tuple[pd.DataFrame, pd.DataFrame]:
@@ -14,29 +15,26 @@ def read_datasets(train_file: str, test_file: str) -> Tuple[pd.DataFrame, pd.Dat
     return train, test
 
 
-def write_datasets(train: pd.DataFrame, test: pd.DataFrame, overwrite: bool, output_train: str, output_test: str) -> Tuple[str, str]:
-    output_train, output_test = get_file_names(overwrite, output_train, output_test)
+def write_datasets(train: pd.DataFrame, test: pd.DataFrame, train_fn: str, test_fn: str) -> Tuple[str, str]:
+    output_train, output_test = get_file_names(train_fn, test_fn)
     train.to_csv(output_train, index=False)
     test.to_csv(output_test, index=False)
     return output_train, output_test
 
 
-def get_file_names(overwrite: bool, output_train: str, output_test: str) -> Tuple[str, str]:
-    if overwrite:
-        return output_train, output_test
-    else:
-        return "tmp_" + output_train, "tmp_" + output_test
+def get_file_names(train: str, test: str) -> Tuple[str, str]:
+    return str(uuid.uuid4()) + '_' + train, str(uuid.uuid4()) + '_' + test
 
 
-def drop_unuseful_columns(train_file: str, test_file: str, unuseful_columns: List[str], overwrite: bool, output_train: str, output_test: str) -> Tuple[str, str]:
+def drop_unuseful_columns(train_file: str, test_file: str, unuseful_columns: List[str]) -> Tuple[str, str]:
     train, test = read_datasets(train_file, test_file)
     train = train.drop(unuseful_columns, axis=1)
     test = test.drop(unuseful_columns, axis=1)
-    output_train, output_test = write_datasets(train, test, overwrite, output_train, output_test)
+    output_train, output_test = write_datasets(train, test, train_file, test_file)
     return output_train, output_test
 
 
-def transform_fields(train_file: str, test_file: str, fields_to_transform: List[str], overwrite: bool, output_train: str, output_test: str) -> Tuple[str, str]:
+def transform_fields(train_file: str, test_file: str, fields_to_transform: List[str]) -> Tuple[str, str]:
     train, test = read_datasets(train_file, test_file)
     if "Age" in fields_to_transform:
         index_nan_age_test = list(test["Age"][test["Age"].isnull()].index)
@@ -70,7 +68,7 @@ def transform_fields(train_file: str, test_file: str, fields_to_transform: List[
     if "Fsize" in fields_to_transform:
         train["Fsize"] = train["SibSp"] + train["Parch"] + 1
         test["Fsize"] = test["SibSp"] + test["Parch"] + 1
-    output_train, output_test = write_datasets(train, test, overwrite, output_train, output_test)
+    output_train, output_test = write_datasets(train, test)
     return output_train, output_test
 
 
@@ -84,22 +82,13 @@ def train_and_predict(train_file: str, test_file: str, field_to_predict: str) ->
     Y_pred = decision_tree.predict(X_test)
     acc_decision_tree = round(decision_tree.score(X_train, Y_train) * 100, 2)
     print("Training Accuracy: {}%".format(acc_decision_tree))
-    return acc_decision_tree, Y_pred
+    return acc_decision_tree #, Y_pred
 
 
 if __name__ == "__main__":
-    command = sys.argv[1]
-
-    # TESTING ###
-    # command = "drop_unuseful_columns"
-    # os.environ["TRAIN_FILE"] = './data/train.csv'
-    # os.environ["TEST_FILE"] = './data/test.csv'
-    # os.environ["UNUSEFUL_COLUMNS"] = "1"
-    # os.environ["UNUSEFUL_COLUMNS0"] = "Cabin"
-    # os.environ["OVERWRITE"] = "0"
-    # os.environ["OUTPUT_TRAIN"] = './data/train_2.csv'
-    # os.environ["OUTPUT_TEST"] = './data/test_2.csv'
-    # FINISH TESTING ###
+    command = os.environ["COMMAND"]
+    if len(sys.argv) > 1:
+        command = sys.argv[1]
 
     functions = {
         "drop_unuseful_columns": drop_unuseful_columns,
@@ -113,10 +102,7 @@ if __name__ == "__main__":
         #     os.environ[f"UNUSEFUL_COLUMNS{i}"] for i in range(int(os.environ["UNUSEFUL_COLUMNS"]))
         # ]
         arg_unuseful_columns = os.environ["UNUSEFUL_COLUMNS"] # Test
-        arg_overwrite = bool(os.environ["OVERWRITE"])
-        arg_output_train = os.environ["OUTPUT_TRAIN"]
-        arg_output_test = os.environ["OUTPUT_TEST"]
-        output = drop_unuseful_columns(arg_train_file, arg_test_file, arg_unuseful_columns, arg_overwrite, arg_output_train, arg_output_test)
+        output = drop_unuseful_columns(arg_train_file, arg_test_file, arg_unuseful_columns)
         print(yaml.dump({"output": list(output)}))
 
     elif command == "transform_fields":
@@ -125,11 +111,8 @@ if __name__ == "__main__":
         arg_fields_to_transform = [
             os.environ[f"FIELDS_TO_TRANSFORM{i}"] for i in range(int(os.environ["FIELDS_TO_TRANSFORM"]))
         ]
-        arg_overwrite = bool(os.environ["OVERWRITE"])
-        arg_output_train = os.environ["OUTPUT_TRAIN"]
-        arg_output_test = os.environ["OUTPUT_TEST"]
-        output = drop_unuseful_columns(arg_train_file, arg_test_file, arg_fields_to_transform, arg_overwrite, arg_output_train, arg_output_test)
-        print(yaml.dump({"output": output}))
+        output = drop_unuseful_columns(arg_train_file, arg_test_file, arg_fields_to_transform)
+        print(yaml.dump({"output": list(output)}))
 
     elif command == "train_and_predict":
         arg_train_file = os.environ["TRAIN_FILE"]
@@ -137,6 +120,5 @@ if __name__ == "__main__":
         arg_field_to_predict = os.environ["FIELD_TO_PREDICT"]
         output = train_and_predict(arg_train_file, arg_test_file, arg_field_to_predict)
         print(yaml.dump({"output": output}))
-    else:
-        print(yaml.dump({"output": (command, "dummy.csv")}))
+
 
--- brane_packages/visualization/main.py
+++ brane_packages/visualization/main.py
@@ -1,29 +1,101 @@
+#!/usr/bin/env python3
 import os
 import sys
 import yaml
-
-from wordcloud import WordCloud
 from typing import List
+import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sns
+import uuid
+plt.style.use("seaborn-whitegrid")
 
 
-def create(words: List[str], file: str) -> str:
-    wordcloud = WordCloud(max_font_size=40).generate(' '.join(words))
-    plt.imshow(wordcloud, interpolation='bilinear')
-    plt.axis("off")
-    plt.savefig(file)
+def read_dataset(data_fn: str) -> pd.DataFrame:
+    data = pd.read_csv(data_fn)
+    return data
 
-    return file
+
+def generate_figure_name() -> str:
+    figure_name = str(uuid.uuid4()) + ".png"
+    return figure_name
+
+
+def plot_distribution(data: str, feature: str, plot_title: str) -> str:
+    data = read_dataset(data)
+    bins = 30
+    hist = True
+    f_size = (7, 7)
+    fig, ax = plt.subplots(figsize=f_size)
+    ax.set_title(plot_title)
+    sns.distplot(data[feature], color="g", bins=bins, ax=ax)
+    file_name = generate_figure_name()
+    plt.savefig(file_name)
+    return file_name
+
+
+def bar_chart_compare(data: str, feature_y: str, feature_1: str, feature_2: str, y_label: str, plot_title: str) -> str:
+    data = read_dataset(data)
+    f_size = (7, 7)
+    plt.figure(figsize=f_size)
+    plt.title(plot_title)
+    sns.barplot(x=feature_1, y=feature_y, hue=feature_2, ci=None, data=data).set_ylabel(y_label)
+    file_name = generate_figure_name()
+    plt.savefig(file_name)
+    return file_name
+
+
+def feature_group_bar_chart(data: str, feature_y_binary: str, feature_y_index: List[str], feature: str, y_label: str, plot_title: str) -> str:
+    data = read_dataset(data)
+    f_size = (7, 7)
+    survived = data[data[feature_y_binary] == 1][feature].value_counts()
+    dead = data[data[feature_y_binary] == 0][feature].value_counts()
+    df_survived_dead = pd.DataFrame([survived, dead])
+    df_survived_dead.index = feature_y_index
+    ax = df_survived_dead.plot(kind="bar", stacked=True, figsize=f_size, rot=0)
+    ax.set_ylabel(y_label)
+    file_name = generate_figure_name()
+    plt.title(plot_title)
+    plt.savefig(file_name)
+    return file_name
 
 
 if __name__ == "__main__":
-    command = sys.argv[1]
-    argument_file = os.environ["FILE"]
-    argument_words = [
-        os.environ[f"WORDS_{i}"] for i in range(int(os.environ["WORDS"]))
-    ]
+    command = os.environ["COMMAND"]
+    if len(sys.argv) > 1:
+        command = sys.argv[1]
+
     functions = {
-        "create": create,
+        "plot_distribution": plot_distribution,
+        "bar_chart_compare": bar_chart_compare,
+        "feature_group_bar_chart": feature_group_bar_chart
     }
-    output = functions[command](argument_words, argument_file)
-    print(yaml.dump({"output": output}))
+    if command == "plot_distribution":
+        arg_data = os.environ["DATA"]
+        arg_feature = os.environ["FEATURE"]
+        arg_plot_title = os.environ["PLOT_TITLE"]
+        output = plot_distribution(arg_data, arg_feature, arg_plot_title)
+        print(yaml.dump({"output": output}))
+
+    elif command == "bar_chart_compare":
+        arg_data = os.environ["DATA"]
+        arg_feature_y = os.environ["FEATURE_Y"]
+        arg_feature_1 = os.environ["FEATURE_1"]
+        arg_feature_2 = os.environ["FEATURE_2"]
+        arg_y_label = os.environ["Y_LABEL"]
+        arg_plot_title = os.environ["PLOT_TITLE"]
+        output = bar_chart_compare(arg_data, arg_feature_y, arg_feature_1, arg_feature_2, arg_y_label, arg_plot_title)
+        print(yaml.dump({"output": output}))
+
+    elif command == "feature_group_bar_chart":
+        arg_data = os.environ["DATA"]
+        arg_feature_y_binary = os.environ["FEATURE_Y_BINARY"]
+        arg_feature = os.environ["FEATURE"]
+        arg_y_label = os.environ["Y_LABEL"]
+        arg_plot_title = os.environ["PLOT_TITLE"]
+        arg_feature_y_index = [
+            os.environ[f"FEATURE_Y_INDEX{i}"] for i in range(int(os.environ["FEATURE_Y_INDEX"]))
+        ]
+        output = feature_group_bar_chart(arg_data, arg_feature_y_binary, arg_feature_y_index, arg_feature, arg_y_label, arg_plot_title)
+        print(yaml.dump({"output": output}))
+
+
